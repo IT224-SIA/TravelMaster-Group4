1811053d4e2d1a808f4c9abbbae1737285624d97
Sheen Ocon <oconsheen@gmail.com>
2024-06-14 19:16:26 +0800

    Last step sa deployment according sa pdf

--- TravelMaster/TravelMaster/bootstrap/app.php
+++ TravelMaster/TravelMaster/bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+(
+    new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
+        dirname(__DIR__)
+    )
+)->bootstrap();
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
@@ -27,6 +29,7 @@ $app->withFacades();
 
 $app->withEloquent();
 $app->configure('services');
+$app->configure('database');
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
@@ -110,7 +113,7 @@ $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
